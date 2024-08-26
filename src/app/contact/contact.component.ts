f4044e150cac630ef20adec40c4b726a1ff8bd3e
bhaveshgoswami02 <bhaveshgoswami2381998@gmail.com>
2024-08-26 16:30:57 +0530

    add state

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,13 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-  fname : any;
-  lname : any;
-  phone : any;
-  work : any;
-  email : any;
-  message : any;
-  
+  fname: any;
+  lname: any;
+  phone: any;
+  work: any;
+  email: any;
+  message: any;
+  state: any = "";
+
   submit() {
 
     const email = 'deepikajeengar939@gmail.com'; // Replace with the recipient's email address
@@ -22,5 +23,5 @@ export class ContactComponent {
     const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
     window.location.href = mailtoLink;
-  } 
+  }
 }
